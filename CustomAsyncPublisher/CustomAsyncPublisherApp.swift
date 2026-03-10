40081b22ebd002aaec62251813c0cc9212bb5ca7
saverio-negro <saverionegro@gmail.com>
2026-03-10 19:03:48 +0100

    Initial Commit

---
+++ CustomAsyncPublisher/CustomAsyncPublisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAsyncPublisherApp.swift
+//  CustomAsyncPublisher
+//
+//  Created by Saverio Negro on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct CustomAsyncPublisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
